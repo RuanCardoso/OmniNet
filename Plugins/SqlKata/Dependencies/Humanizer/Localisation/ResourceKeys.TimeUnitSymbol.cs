280bfb4ade3841967f7345710943daf3706a7f10
Ruan Cardoso <61716126+RuanCardoso@users.noreply.github.com>
2022-09-04 03:11:10 -0300

    Updated Humanizer and Dappeer

---
+++ Plugins/SqlKata/Dependencies/Humanizer/Localisation/ResourceKeys.TimeUnitSymbol.cs
@@ -0,0 +1,26 @@
+﻿namespace Humanizer.Localisation
+{
+    public partial class ResourceKeys
+    {
+        /// <summary>
+        /// Encapsulates the logic required to get the resource keys for TimeUnit.ToSymbol
+        /// </summary>
+        public static class TimeUnitSymbol
+        {
+            /// <summary>
+            /// Examples: TimeUnit_Minute, TimeUnit_Hour.
+            /// </summary>
+            private const string TimeUnitFormat = "TimeUnit_{0}";
+
+            /// <summary>
+            /// Generates Resource Keys according to convention.
+            /// </summary>
+            /// <param name="unit">Time unit, <see cref="TimeUnit"/>.</param>
+            /// <returns>Resource key, like TimeSpanHumanize_SingleMinute</returns>
+            public static string GetResourceKey(TimeUnit unit)
+            {
+                return TimeUnitFormat.FormatWith(unit);
+            }
+        }
+    }
+}
