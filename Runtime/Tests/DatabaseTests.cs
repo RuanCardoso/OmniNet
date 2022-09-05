96189ce31930433a0e3cfb18ac1ba1533d98074a
Ruan Cardoso <61716126+RuanCardoso@users.noreply.github.com>
2022-09-05 01:11:19 -0300

    Update DatabaseTests.cs

--- Runtime/Tests/DatabaseTests.cs
+++ Runtime/Tests/DatabaseTests.cs
@@ -1,16 +1,27 @@
 using Neutron.Core;
+using Neutron.Database;
 using UnityEngine;
 
 public class DatabaseTests : MonoBehaviour
 {
-    private SGBDManager Manager = new((db) => db.Initialize("Users"), 5);
+    private static string address = "ofgame.c0mgaov7q9jm.sa-east-1.rds.amazonaws.com";
+    private static string database = "SevenDayOfSurvival";
+    private static string username = "admin";
+    private static string password = "ofGame945173!";
+    private SGBDManager Manager = new((db) => db.Initialize("Users", SGDBType.MySQL, $"Server={address};Database={database};Uid={username};Pwd={password};"), 5);
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            // var dB = Manager.Get().Db;
-            // dB.Insert();
-            // Manager.Release(dB);
+            for (int i = 0; i < 5; i++)
+            {
+                var dB = Manager.Get();
+                dB.Db.Insert(new
+                {
+                    Name = "Ruan",
+                });
+                Manager.Release(dB);
+            }
         }
     }
 }
