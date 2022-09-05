727332ea0b628d23aaec140ccea551d690715326
Ruan Cardoso <61716126+RuanCardoso@users.noreply.github.com>
2022-09-05 02:17:12 -0300

    Update DatabaseTests.cs

--- Runtime/Tests/DatabaseTests.cs
+++ Runtime/Tests/DatabaseTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Neutron.Core;
 using Neutron.Database;
@@ -5,25 +6,33 @@ using UnityEngine;
 
 public class DatabaseTests : MonoBehaviour
 {
-    private static string address = "ofgame.c0mgaov7q9jm.sa-east-1.rds.amazonaws.com";
-    private static string database = "SevenDayOfSurvival";
-    private static string username = "admin";
-    private static string password = "ofGame945173!";
-    private SGBDManager Manager = new((db) => db.Initialize("Users", SGDBType.MySQL, $"Server={address};Database={database};Uid={username};Pwd={password};"), 5);
+    private static string address = "localhost";
+    private static string database = "usersdb";
+    private static string username = "root";
+    private static string password = "";
+    private SGBDManager Manager = new((db) => db.Initialize("Users", SGDBType.MariaDB, $"Server={address};Database={database};Uid={username};Pwd={password};"), 5);
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 10000; i++)
             {
                 Task.Run(() =>
                 {
-                    var dB = Manager.Get();
-                    dB.Db.Insert(new
+                    try
                     {
-                        Name = "Ruan",
-                    });
-                    Manager.Release(dB);
+                        var dB = Manager.Get();
+                        int affectedRows = dB.Db.Insert(new
+                        {
+                            Name = "Ruan",
+                        }, null, 1);
+                        Neutron.Core.Logger.Print("Flushed");
+                        Manager.Release(dB);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.LogException(ex);
+                    }
                 });
             }
         }
