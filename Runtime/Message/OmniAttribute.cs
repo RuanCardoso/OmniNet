521cc15537da11be77f42ac518804d4107bf3ce4
Ruan Cardoso <61716126+RuanCardoso@users.noreply.github.com>
2023-12-09 09:14:33 -0400

    Added OmniAttribute

---
+++ Runtime/Message/OmniAttribute.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Omni.Core
+{
+	public class OmniAttribute : Attribute
+	{
+
+	}
+}
--- Runtime/Tests/RemoteTests.cs
+++ Runtime/Tests/RemoteTests.cs
@@ -1,61 +1,60 @@
 using Omni.Core;
-using UnityEngine;
 
 namespace Omni.Tests
 {
-    public partial class RemoteTests : OmniObject
-    {
-        protected override void Start()
-        {
-            base.Start();
-        }
+	public partial class RemoteTests : OmniObject
+	{
+		//public void RemoteEg(DataIOHandler IOHandler, ushort fromId, ushort toId, RemoteStats stats)
+		//{
+		//	throw new System.NotImplementedException();
+		//}
 
-        private void Update()
-        {
-            if (IsMine)
-            {
-                if (Input.GetKeyDown(KeyCode.A))
-                {
-                    var IOHandler = Get;
-                    IOHandler.Write("Mensagem de teste! Testando o envio de mensagens remotas! Ebaaa!");
-                    Remote(1, IOHandler, Enums.DataDeliveryMode.SecuredWithAes, Enums.DataTarget.Self, Enums.DataProcessingOption.ProcessOnServer, Enums.DataCachingOption.Overwrite);
-                }
+		protected override void Start()
+		{
+			base.Start();
+		}
 
-                if (Input.GetKeyDown(KeyCode.R))
-                {
-                    var IOHandler = Get;
-                    IOHandler.Write("Mensagem de teste! Testando o envio de mensagens remotas! Ebaaa!");
-                    Remote(1, IOHandler, Enums.DataDeliveryMode.Secured, Enums.DataTarget.Self, Enums.DataProcessingOption.ProcessOnServer, Enums.DataCachingOption.Overwrite);
-                }
+		private void Update()
+		{
+			//if (IsMine)
+			//{
+			//    if (Input.GetKeyDown(KeyCode.A))
+			//    {
+			//        var IOHandler = Get;
+			//        IOHandler.Write("Mensagem de teste! Testando o envio de mensagens remotas! Ebaaa!");
+			//        Remote(1, IOHandler, Enums.DataDeliveryMode.SecuredWithAes, Enums.DataTarget.Self, Enums.DataProcessingOption.ProcessOnServer, Enums.DataCachingOption.Overwrite);
+			//    }
 
-                for (int i = 0; i < 0; i++)
-                {
-                    //if (Input.GetKeyDown(KeyCode.U))
-                    {
-                        var IOHandler = Get;
-                        IOHandler.WriteWithoutAllocation("Mensagem de teste! Testando o envio de mensagens remotas! Ebaaa!");
-                        Remote(1, IOHandler, Enums.DataDeliveryMode.Unsecured, Enums.DataTarget.BroadcastExcludingSelf, Enums.DataProcessingOption.ProcessOnServer, Enums.DataCachingOption.Overwrite);
-                    }
-                }
-            }
-        }
+			//    if (Input.GetKeyDown(KeyCode.R))
+			//    {
+			//        var IOHandler = Get;
+			//        IOHandler.Write("Mensagem de teste! Testando o envio de mensagens remotas! Ebaaa!");
+			//        Remote(1, IOHandler, Enums.DataDeliveryMode.Secured, Enums.DataTarget.Self, Enums.DataProcessingOption.ProcessOnServer, Enums.DataCachingOption.Overwrite);
+			//    }
 
-        [Remote(1)]
-        partial void RPC1(DataIOHandler IOHandler, ushort fromId, ushort toId, RemoteStats stats);
-    }
-}
+			//    for (int i = 0; i < 0; i++)
+			//    {
+			//        //if (Input.GetKeyDown(KeyCode.U))
+			//        {
+			//            var IOHandler = Get;
+			//            IOHandler.WriteWithoutAllocation("Mensagem de teste! Testando o envio de mensagens remotas! Ebaaa!");
+			//            Remote(1, IOHandler, Enums.DataDeliveryMode.Unsecured, Enums.DataTarget.BroadcastExcludingSelf, Enums.DataProcessingOption.ProcessOnServer, Enums.DataCachingOption.Overwrite);
+			//        }
+			//    }
+			//}
+		}
 
-// Definindo uma classe com um método parcial
-public partial class Exemplo
-{
-    // Parte 1 do método
-    public partial void ExibirMensagem(string mensagem);
+		//      [Remote(1)]
+		//      partial void RemoteEg(DataIOHandler IOHandler, ushort fromId, ushort toId, RemoteStats stats);
 
-    // Parte 2 do método
-    public partial void ExibirMensagem(string mensagem)
-    {
-       
-    }
+		//public override void RemoteEgServerLogic(DataIOHandler IOHandler, ushort fromId, ushort toId, RemoteStats stats)
+		//{
+		//	base.RemoteEgServerLogic(IOHandler, fromId, toId, stats);
+		//}
 
-    // Outros membros da classe...
+		//public override void RemoteEgClientLogic(DataIOHandler IOHandler, ushort fromId, ushort toId, RemoteStats stats)
+		//{
+		//	base.RemoteEgClientLogic(IOHandler, fromId, toId, stats);
+		//}
+	}
 }
