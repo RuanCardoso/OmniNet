42d9e19a238fbad45a6025bbd98bc1bfbd8b81b5
Ruan Cardoso <61716126+RuanCardoso@users.noreply.github.com>
2024-03-01 02:55:26 -0400

    Added TryAdd and Add to properties

--- Runtime/Database/Row.cs
+++ Runtime/Database/Row.cs
@@ -1,54 +1,56 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace Omni.Core
 {
-    public class Row : Dictionary<string, object>
-    {
-        public new object this[string key] => base[key];
-        public T Get<T>(string key) => (T)Convert.ChangeType(base[key], typeof(T));
-        public T FastGet<T>(string key)
-        {
-            object value = base[key];
-            return Unsafe.As<object, T>(ref value);
-        }
+	public class Row : Dictionary<string, object>
+	{
+		public new object this[string key] => base[key];
+		public T Get<T>(string key) => (T)Convert.ChangeType(base[key], typeof(T));
+		public T FastGet<T>(string key)
+		{
+			object value = base[key];
+			return Unsafe.As<object, T>(ref value);
+		}
 
-        public bool TryGet<T>(string key, out T prop)
-        {
-            prop = default;
-            try
-            {
-                if (TryGetValue(key, out object value))
-                {
-                    prop = (T)Convert.ChangeType(value, typeof(T));
-                    return true;
-                }
-                return false;
-            }
-            catch
-            {
-                return false;
-            }
-        }
+		public bool TryGet<T>(string key, out T prop)
+		{
+			prop = default;
+			try
+			{
+				if (TryGetValue(key, out object value))
+				{
+					prop = (T)Convert.ChangeType(value, typeof(T));
+					return true;
+				}
+				return false;
+			}
+			catch
+			{
+				return false;
+			}
+		}
 
-        public bool TryFastGet<T>(string key, out T prop)
-        {
-            prop = default;
-            try
-            {
-                if (TryGetValue(key, out object value))
-                {
-                    prop = Unsafe.As<object, T>(ref value);
-                    return true;
-                }
-                return false;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-    }
+		public bool TryFastGet<T>(string key, out T prop)
+		{
+			prop = default;
+			try
+			{
+				if (TryGetValue(key, out object value))
+				{
+					prop = Unsafe.As<object, T>(ref value);
+					return true;
+				}
+				return false;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public new void Add(string key, object value) => base.Add(key, value);
+		public new bool TryAdd(string key, object value) => base.TryAdd(key, value);
+	}
 }
